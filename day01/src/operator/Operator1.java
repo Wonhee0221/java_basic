5c66bc573f2be62fd3bee2a25685f58d6eded271
Wonhee0221 <totohwh3601@naver.com>
2024-03-07 16:39:57 +0900

    2024-03-07(if문)

--- day01/src/operator/Operator1.java
+++ day01/src/operator/Operator1.java
@@ -11,5 +11,44 @@ public class Operator1 {
         double i3 =i1*1.0/i2;
         System.out.println(i3);
 
+        int result = i1++ + i2;
+        System.out.printf("%d %d %d",result,i1,i2);
+
+        int a1 = 0;
+        if (3>5) {
+            a1 = 8;
+        }
+        else {
+            a1= 9;
+        }
+        int bb = 3>5?6:9;
+        System.out.printf("%d,%d",a1, bb);
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        // 3수의 최대값을 구하시오
+        int num4 = 0;
+        num4 = (num1>num2)?((num1 > num3)?num1 : num3):((num2>num3)?num2:num3);
+        System.out.println();
+        System.out.printf("fffff44f %d",num4);
+        if (num1>num2){
+            if (num1 > num3){
+                num4 = num1;
+            }
+            else {
+                num4 = num3;
+            }
+        }
+        else {
+            if(num2>num3){
+                num4 = num2;
+            }
+            else {
+                num4 = num3;
+            }
+        }
+        System.out.printf("ffffff %d",num4);
     }
 }
