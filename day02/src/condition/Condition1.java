ba8a1a60c29ff087ca09aef2d40198b38f7171f4
Wonhee0221 <totohwh3601@naver.com>
2024-03-08 10:13:48 +0900

    24-03-08(중간저장)

--- day02/src/condition/Condition1.java
+++ day02/src/condition/Condition1.java
@@ -4,9 +4,9 @@ public class Condition1 {
     public static void main(String[] args){
         int a = 10;
         System.out.println("Start...");
-        System.out.println("Connectt...");
+        System.out.println("Connect...");
         if (a<20){
-            System.out.println("Disconnect..");
+//            System.out.println("Disconnect..");
             System.out.println("End,,,");
             return;
         }
