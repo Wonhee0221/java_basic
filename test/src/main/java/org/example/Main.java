2671e47b3eda8625258f4d1ed6fd64357c3686d4
Wonhee0221 <totohwh3601@naver.com>
2024-03-15 17:02:56 +0900

    24-03-15(test모듈 추가)

---
+++ test/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.ConnectionPool;
+
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ConnectionPool cp = ConnectionPool.create();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
